79aee2a3ed53967cb543c7b2d5a66fec9d2f9657
ClaheEventos <personaldeit@claheeventos.com>
2025-07-17 11:20:52 -0300

    Mejora

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Logo from './components/Logo'; // Mostrará logo.png desde public/
 import RegistroUniforme from './components/RegistroUniforme';
+
 function App() {
-  return <RegistroUniforme />;
+  return (
+    <div className="App">
+      <Logo /> {/* Mostrar el logo arriba */}
+      <RegistroUniforme /> {/* Mostrar el formulario debajo */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <img
+        src="./public/logo.png"
+        alt="Logo"
+        style={{ width: '200px', height: 'auto' }}
+      />
+    </div>
+  );
+}
+
+export default Logo;
